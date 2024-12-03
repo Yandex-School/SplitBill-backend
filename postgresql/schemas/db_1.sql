b27ef84359b453c4662796b3a1308f3a91b28ec8
SayMax16 <saydulloismatovvv@gmail.com>
2024-12-03 20:13:38 +0500

    Removed enum type declaration and changed status column from user_products table to varchar data type

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,21 +1,6 @@
 DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA IF NOT EXISTS public;
 
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_type
-        WHERE typname = 'paid_status'
-    ) THEN
-CREATE TYPE paid_status AS ENUM (
-            'PAID',
-            'UNPAID'
-        );
-END IF;
-END $$;
-
-
 CREATE TABLE IF NOT EXISTS users
 (
     id        serial4 PRIMARY KEY,
@@ -49,7 +34,7 @@ CREATE TABLE IF NOT EXISTS products
 CREATE TABLE IF NOT EXISTS user_products
 (
     id         serial4 PRIMARY KEY,
-    status     paid_status DEFAULT 'UNPAID',
+    status     varchar(255) DEFAULT 'UNPAID',
     product_id int4 REFERENCES products(id) ON DELETE CASCADE NOT NULL,
     user_id    int4 REFERENCES users(id) ON DELETE CASCADE NOT NULL
 );
